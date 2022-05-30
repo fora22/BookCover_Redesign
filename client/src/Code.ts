891c2627235eb650ce8cdddd292277bc938210b3
FoRA <48875566+fora22@users.noreply.github.com>
2022-05-30 23:21:17 +0900

    include func make

--- client/src/Code.ts
+++ client/src/Code.ts
@@ -2,3 +2,7 @@ function doGet(e) {
   var index = HtmlService.createTemplateFromFile("src/index");
   return index.evaluate();
 }
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
