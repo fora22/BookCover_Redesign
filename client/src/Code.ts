f96a95602a4fca30b0193fc5bc7bdeefda08dfb6
FoRA <48875566+fora22@users.noreply.github.com>
2022-06-12 16:04:00 +0900

    img 추가 및 타이틀 설정 완료

--- client/src/Code.ts
+++ client/src/Code.ts
@@ -1,6 +1,6 @@
 function doGet(e) {
   var index = HtmlService.createTemplateFromFile("src/index");
-  return index.evaluate();
+  return index.evaluate().setTitle("나뚜 Docs");
 }
 
 function include(filename) {
--- client/src/serverSide.ts
+++ client/src/serverSide.ts
@@ -23,6 +23,35 @@ function get_sheet_info (sheet) {
     }
 }
 
+function get_images(RGB_or_CMYK) {
+  // RGB_or_CMYK = "CMYK";
+  const scriptProperties = PropertiesService.getScriptProperties();
+  const BOOKCOVER_IMGS_FOLDER_NAME = scriptProperties.getProperty('BOOKCOVER_IMGS_FOLDER_NAME');
+  console.log(BOOKCOVER_IMGS_FOLDER_NAME);
+  
+  let url_json = {};
+  let file;
+  let file_name = "";
+  let file_name_list;
+  let download_url = "";
+  
+  let file_iterator = DriveApp.getFoldersByName(BOOKCOVER_IMGS_FOLDER_NAME).next().getFilesByType(MimeType.PNG);
+  
+  while (file_iterator.hasNext()) {
+    file = file_iterator.next();
+    download_url = file.getDownloadUrl().replace("download", "view");
+    file_name = file.getName();
+    file_name_list = file_name.split("_");
+    
+    if (file_name_list[2] === RGB_or_CMYK) {
+      url_json[file_name] = download_url;
+      }
+  }
+    
+  // console.log(url_json);
+  return url_json;
+}
+
 function get_book_info() {
   const book_info_sheet = conn_to_sheet('BOOKCOVER_INFO_SHEET_NAME');
 
@@ -30,6 +59,8 @@ function get_book_info() {
 
   const range = book_info_sheet.getRange(2,1,sheet_info.last_row - 1, sheet_info.last_col);
   const book_info = range.getDisplayValues();
+  const color_type = "CMYK";
+  let book_images_json = get_images(color_type);
 
   let book_info_json = {};
   book_info.forEach((b_info) => {
@@ -43,10 +74,14 @@ function get_book_info() {
             publisher: b_info[4],   // 출판사
             date_publish: b_info[5],   // 출판일
             link: b_info[8],        // 책 링크
+            recover_front_link: book_images_json[`${b_info[0]}_RECOVER_${color_type}_FRONT.png`], // recover front img link
+            recover_back_link: book_images_json[`${b_info[0]}_RECOVER_${color_type}_BACK.png`],   // recover back img link
+            origin_front_link: book_images_json[`${b_info[0]}_ORIGIN_${color_type}_FRONT.png`],   // origin front img link
+            origin_back_link: book_images_json[`${b_info[0]}_ORIGIN_${color_type}_BACK.png`],     // origin back img link
         }
     }
   })
 
-//   console.log(book_info_json);
+  //   console.log(book_info_json);
   return JSON.stringify(book_info_json)
 }
